850a7725a8c04c0dc28c45a75c946d0dbcd1030c
vladp <de5pot162@gmail.com>
2021-09-09 23:07:28 +0300

    create sortPopup

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Categories({items, onClick}) {
+function Categories({items}) {
+  const [activeItem, setActiveItem] = useState(null);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  }
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        {items.map((item) => {
-          return <li onClick={() => onClick(item)} key={item}>{item}</li>
-        })}
+        <li className={activeItem === null ? 'active' : ''}
+            onClick={() => onSelectItem(null)}
+        >Все</li>
+
+        {items && items.map((item, index) => (
+          <li className={activeItem === index ? 'active' : ''}
+              onClick={() => onSelectItem(index)}
+              key={item}
+          >{item}</li>
+        ))}
       </ul>
     </div>
   );
