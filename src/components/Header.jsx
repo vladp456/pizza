183eae99741c8ce3b07c8bdbb1868940f6187ffc
vladp <de5pot162@gmail.com>
2021-09-10 14:24:25 +0300

    create Home, Cart, add Router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import logoSvg from "../assets/img/pizza-logo.svg";
 import Button from "./Button";
+import {Link} from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
-          <img width="38" src={logoSvg} alt="Pizza logo"/>
-
-          <div>
-            <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+        <Link to="/">
+          <div className="header__logo">
+            <img width="38" src={logoSvg} alt="Pizza logo"/>
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
           </div>
-        </div>
+        </Link>
 
-        <Button />
+        <Link to="/cart">
+          <Button />
+        </Link>
       </div>
     </div>
   );
