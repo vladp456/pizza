1e84072506bca1a0ca1c2e51db47f049164f5629
vladp <de5pot162@gmail.com>
2021-09-10 20:53:05 +0300

    create PizzaBlock

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import {Route} from "react-router-dom";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json').then((res) => res.json()).then(json => {
+      setPizzas(json.pizzas);
+    })
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
 
       <div className="content">
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" render={() => <Home items={pizzas} />} />
         <Route exact path="/cart" component={Cart} />
       </div>
     </div>
