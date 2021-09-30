c18f3d32c9b435ff610f91b6d55001d09bf75967
vladp <de5pot162@gmail.com>
2021-09-30 16:47:56 +0300

    fix issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {Route} from "react-router-dom";
-import {useDispatch} from "react-redux";
 
 import Header from "./components/Header";
 import Home from "./pages/Home";
